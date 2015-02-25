a848bb5b47ad05318a4e03b8f59749f2bfbc3c28
mb <m2b@gmail.com>
2015-02-25 20:42:08 +0100

    Little change.

---
+++ service/src/main/java/pl/zg/cart/service/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+package pl.zg.cart.service;
+
+import pl.zg.cart.model.Customer;
+
+/**
+ * Created by mb on 2015-02-25 20:14.
+ */
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer register(Customer customer) {
+        //
+        return null;
+    }
+}
